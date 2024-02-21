7292fb18e7184559adffc9d6ee0bbee0c898e843
xSpookyDev <ricardo13dev@gmail.com>
2024-02-21 03:33:52 -0300

    ok

---
+++ nivel 1/script.js
@@ -0,0 +1,15 @@
+"use strict";
+var pesoKg = 80;
+document.write(pesoKg+ " Kilos <br>");
+var pesoKg=82;
+document.write(pesoKg + " Kilos <br>")
+
+ var edad= 23;
+document.write(edad+ "<br>")
+
+edad= 25;
+document.write(edad + "<br>")
+
+//let reasignable no redeclarable 
+
+let 
